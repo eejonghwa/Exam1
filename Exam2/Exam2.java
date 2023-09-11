51fdcafa3afa4f1a8494f09c2ffb659ba0841f03
eejonghwa <teriyaki970326@gamil.com>
2023-09-11 17:06:12 +0900

    자바기초시험

---
+++ Exam2/Exam2.java
@@ -0,0 +1,92 @@
+package Exam2;
+
+public class Exam2 {
+
+    public static void main(String[] args) {
+        int high = 3;
+        int linenum;
+
+        for(int j =0; j < high; j++){
+            System.out.println();
+            for(int i = 0; i < high; i++){
+                System.out.print("*");
+            }
+        }
+
+
+
+
+
+
+//        for(int i = 0; i < 1; i++){
+//            System.out.print("*");
+//        }
+//        System.out.println();
+//
+//        for(int i = 0; i < 3; i++){
+//            System.out.print("*");
+//        }
+//        System.out.println();
+//
+//        for(int j = 0; j < 5; j++){
+//            System.out.print("*");
+//        }
+
+
+
+
+        // 높이를 입력해주세요 : 3                // 출력
+
+        /*
+
+         *
+
+         ***
+
+         *****
+
+         */
+
+        // 높이를 입력해주세요 : 5
+
+        // 출력
+
+        /*
+
+         *
+
+         ***
+
+         *****
+
+         *******
+
+         *********
+
+         */
+
+        // 높이를 입력해주세요 : 7
+
+        // 출력
+
+        /*
+
+         *
+
+         ***
+
+         *****
+
+         *******
+
+         *********
+
+         ***********
+
+         *************
+
+         */
+
+    }
+
+}
